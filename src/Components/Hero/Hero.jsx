df93f7b32e7d178491402cb529852afb6f7cff4f
bibekkunwar <96648243+bibekonchain@users.noreply.github.com>
2026-02-04 22:15:19 +0545

    Refactor Hero component layout and styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import './Hero.css'
-import dark_arrow from '../../assets/dark-arrow.png'
+import React from 'react';
+import './Hero.css';
+import dark_arrow from '../../assets/dark-arrow.png';
+import heroBg from '../../assets/hero.png';
 
 const Hero = () => {
   return (
-    <div className='hero container'>
-      <div className="hero-text">
-        <h1>We Ensure better education for a better world</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Hic tempore nihil ipsa vel dicta dolorem velit quia quasi sint,
-          atque, suscipit quae consequatur eligendi vero. 
-           Molestiae maxime magni minus doloribus.</p>
-           <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+    <section className='hero'>
+      <div className="overlay"></div>
+      <div className="hero-content container">
+        <h1>We Ensure Better Education for a Better World</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic tempore nihil ipsa vel dicta dolorem
+          velit quia quasi sint, atque, suscipit quae consequatur eligendi vero. Molestiae maxime magni minus
+          doloribus.
+        </p>
+        <button className='btn'>
+          Explore More
+          <span className="arrow">
+            <img src={dark_arrow} alt="arrow" />
+          </span>
+        </button>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
