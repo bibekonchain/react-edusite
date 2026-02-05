c80c4ae97cee404c17cbcaa03acfb69142608dcc
bibekkunwar <96648243+bibekonchain@users.noreply.github.com>
2026-02-05 22:49:44 +0545

    Refactor About component for improved readability

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import './About.css'
-import about_img from '../../assets/about.png'
-import play_icon from '../../assets/play-icon.png'
+import React from "react";
+import "./About.css";
+import about_img from "../../assets/about.png";
+import play_icon from "../../assets/play-icon.png";
 
 const About = () => {
   return (
-    <div className='about'>
-        <div className="about-left">
-            <img src={about_img} alt="" className='about-img'/>
-            <img src={play_icon} alt="" className='play-icon'/>
-        </div>
-        <div className="about-right">
-            <h3>ABOUT UNIVERSITY</h3>
-            <h2>Leading The Future, For the Future</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Similique excepturi quas fugit quaerat quia corporis a beatae est molestias nemo 
-                enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae necessitatibus.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Similique excepturi quas fugit quaerat quia corporis a beatae est molestias nemo 
-                enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae necessitatibus.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Similique excepturi quas fugit quaerat quia corporis a beatae est molestias nemo 
-                enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae necessitatibus.</p>
-        </div>
-    </div>
-  )
-}
+    <div className="about">
+      <div className="about-left">
+        <img src={about_img} alt="University Campus" className="about-img" />
 
-export default About
+        <img
+          src={play_icon}
+          alt="Play video"
+          className="play-icon"
+        />
+      </div>
+
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Leading The Future, For the Future</h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+          excepturi quas fugit quaerat quia corporis a beatae est molestias nemo
+          enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae
+          necessitatibus.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+          excepturi quas fugit quaerat quia corporis a beatae est molestias nemo
+          enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae
+          necessitatibus.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+          excepturi quas fugit quaerat quia corporis a beatae est molestias nemo
+          enim, laudantium, minima, quod aliquid nostrum laboriosam unde quae
+          necessitatibus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
