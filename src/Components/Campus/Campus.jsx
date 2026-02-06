67bdb5ef125390f7d024bf496741cab4f60b4c6f
bibekkunwar <96648243+bibekonchain@users.noreply.github.com>
2026-02-06 23:33:52 +0545

    Refactor Campus component structure and alt text
    Updated the Campus component to use a section element and improved alt text for images.

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -6,18 +6,21 @@ import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
 import white_arrow from '../../assets/white-arrow.png'
 
-
 const Campus = () => {
   return (
-    <div className='Campus'>
-        <div className="gallery">
-            <img src={gallery_1} alt="" />
-            <img src={gallery_2} alt="" />
-            <img src={gallery_3} alt="" />
-            <img src={gallery_4} alt="" />
-        </div>
-    <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
-    </div>
+    <section className="Campus">
+      <div className="gallery">
+        <img src={gallery_1} alt="Campus view 1" />
+        <img src={gallery_2} alt="Campus view 2" />
+        <img src={gallery_3} alt="Campus view 3" />
+        <img src={gallery_4} alt="Campus view 4" />
+      </div>
+
+      <button className="btn dark-btn">
+        See more here
+        <img src={white_arrow} alt="arrow" />
+      </button>
+    </section>
   )
 }
 
