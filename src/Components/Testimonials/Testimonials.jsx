10898ddf92ea07097916592bbd98dde9cc1e4c72
bibekkunwar <96648243+bibekonchain@users.noreply.github.com>
2026-02-08 23:21:08 +0545

    Refactor Testimonials slider logic and structure
    Refactor slider functionality to use refs for tracking position and improve mobile responsiveness.

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -8,108 +8,102 @@ import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
+  const slider = useRef(null)
+  const tx = useRef(0)
 
-    const slider = useRef();
-    let tx = 0;
-    
+  const isMobile = () => window.innerWidth <= 768
 
-const slideForward = () =>{
+  const slideForward = () => {
+    const step = isMobile() ? 25 : 25   // keep same step, but mobile uses different widths in CSS
+    const minTx = isMobile() ? -75 : -50
 
-if(tx > -50){
-    tx -= 25;
-}
-slider.current.style.transform =  `translateX(${tx}%)`
-
-}
-
-const slideBackward = () =>{
-    
-
-    if(tx < 0){
-        tx += 25;
+    if (tx.current > minTx) {
+      tx.current -= step
+      slider.current.style.transform = `translateX(${tx.current}%)`
     }
-    slider.current.style.transform =  `translateX(${tx}%)`
-    
+  }
 
-}
+  const slideBackward = () => {
+    const step = isMobile() ? 25 : 25
+
+    if (tx.current < 0) {
+      tx.current += step
+      slider.current.style.transform = `translateX(${tx.current}%)`
+    }
+  }
 
   return (
     <div className='testimonials'>
-        <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
-        <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
-        <div className="slider">
-            <ul ref={slider}>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_1} alt="" />
-                            <div>
-                                <h3>Eva Jackson</h3>
-                                <span>Chitwan, Nepal</span>
-                            </div>
-                        </div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Maiores, iste tenetur neque repudiandae ullam nobis commodi? 
-                            Perspiciatis, magnam necessitatibus? Voluptatem excepturi aliquam 
-                            ea reprehenderit, delectus sequi nostrum quisquam laudantium! Assumenda!
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_2} alt="" />
-                            <div>
-                                <h3>Eva Jackson</h3>
-                                <span>Chitwan, Nepal</span>
-                            </div>
-                        </div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Maiores, iste tenetur neque repudiandae ullam nobis commodi? 
-                            Perspiciatis, magnam necessitatibus? Voluptatem excepturi aliquam 
-                            ea reprehenderit, delectus sequi nostrum quisquam laudantium! Assumenda!
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_3} alt="" />
-                            <div>
-                                <h3>Eva Jackson</h3>
-                                <span>Chitwan, Nepal</span>
-                            </div>
-                        </div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Maiores, iste tenetur neque repudiandae ullam nobis commodi? 
-                            Perspiciatis, magnam necessitatibus? Voluptatem excepturi aliquam 
-                            ea reprehenderit, delectus sequi nostrum quisquam laudantium! Assumenda!
-                        </p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_4} alt="" />
-                            <div>
-                                <h3>Eva Jackson</h3>
-                                <span>Chitwan, Nepal</span>
-                            </div>
-                        </div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Maiores, iste tenetur neque repudiandae ullam nobis commodi? 
-                            Perspiciatis, magnam necessitatibus? Voluptatem excepturi aliquam 
-                            ea reprehenderit, delectus sequi nostrum quisquam laudantium! Assumenda!
-                        </p>
-                    </div>
-                </li>
-            </ul>
-        </div>
+      <img src={next_icon} alt="Next" className='next-btn' onClick={slideForward} />
+      <img src={back_icon} alt="Back" className='back-btn' onClick={slideBackward} />
 
+      <div className="slider">
+        <ul ref={slider}>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="Eva Jackson" />
+                <div>
+                  <h3>Eva Jackson</h3>
+                  <span>Chitwan, Nepal</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iste tenetur
+                neque repudiandae ullam nobis commodi?
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="Eva Jackson" />
+                <div>
+                  <h3>Eva Jackson</h3>
+                  <span>Chitwan, Nepal</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iste tenetur
+                neque repudiandae ullam nobis commodi?
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="Eva Jackson" />
+                <div>
+                  <h3>Eva Jackson</h3>
+                  <span>Chitwan, Nepal</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iste tenetur
+                neque repudiandae ullam nobis commodi?
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="Eva Jackson" />
+                <div>
+                  <h3>Eva Jackson</h3>
+                  <span>Chitwan, Nepal</span>
+                </div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iste tenetur
+                neque repudiandae ullam nobis commodi?
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
